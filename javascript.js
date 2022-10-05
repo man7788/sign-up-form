50947856fb4b18a2b667d898b733bf67638c923a
man7788 <jerman1841@gmail.com>
2022-10-06 03:43:04 +0800

    Add form styles

---
+++ javascript.js
@@ -0,0 +1,8 @@
+// const ps = document.querySelector('input[type="password"]');
+// const conps = document.querySelector('input[type="firm-password"]');
+
+// console.log(ps.value);
+
+// function matchPassword() {
+//   if (
+// }
