e237e46d984e3704d5aef45f474dc3b93a978c89
man7788 <jerman1841@gmail.com>
2022-10-07 03:24:12 +0800

    Add password validation

--- javascript.js
+++ javascript.js
@@ -1,8 +1,81 @@
-// const ps = document.querySelector('input[type="password"]');
-// const conps = document.querySelector('input[type="firm-password"]');
+const ps = document.querySelector('input[name="password"]');
+const conps = document.querySelector('input[name="firm-password"]');
+const button = document.querySelector('button[type="submit"]');
+const match = document.querySelector('.match');
 
-// console.log(ps.value);
+function passwordErrorStyle() {
+  match.style.color = 'crimson';
+  match.style.fontSize = '0.45rem';
+  ps.style.border = '1px solid crimson';
+  conps.style.border = '1px solid crimson';
+};
 
-// function matchPassword() {
-//   if (
-// }
+function passwordEmpty() {
+  match.textContent = '*Passwords cannot be empty';
+  passwordErrorStyle();
+  removeFocusOut();
+};
+
+function passwordNotMatch() {
+  match.textContent = '*Passwords do not match';
+  passwordErrorStyle();
+  removeFocusOut();
+  removeFocus();
+};
+
+function passwordMatch() {
+  match.textContent = '';
+  ps.style.border = '1px solid #E5E7EB';
+  conps.style.border = '1px solid #E5E7EB';
+  addFocusOut();
+  addFocus();
+};
+
+function focus() {
+  this.style.border = '1px solid dodgerblue';
+};
+
+function removeFocus() {
+  ps.removeEventListener('focus', focus);
+  conps.removeEventListener('focus', focus);
+};
+
+function removeFocusOut() {
+  ps.removeEventListener('focusout', passwordMatch);
+  conps.removeEventListener('focusout', passwordMatch);
+};
+
+function addFocus() {
+  ps.addEventListener('focus', focus);
+  conps.addEventListener('focus', focus);
+};
+
+function addFocusOut() {
+  ps.addEventListener('focusout', passwordMatch);
+  conps.addEventListener('focusout', passwordMatch);
+};
+
+function checkPassword() {
+    ps.addEventListener('input', checkPassword);
+    if (ps.value === '' && conps.value === '') {
+      passwordEmpty();
+      button.type = 'button';
+      return;
+    } else if (ps.value !== conps.value) {
+      passwordNotMatch();
+      button.type = 'button';
+      return;
+    } else if (ps.value === conps.value) {
+      passwordMatch();
+      button.type = 'submit';
+    };
+};
+
+button.addEventListener('click', checkPassword);
+
+ps.addEventListener('focus', focus);
+ps.addEventListener('focusout', passwordMatch);
+
+conps.addEventListener('focus', focus);
+conps.addEventListener('focusout', passwordMatch);
+conps.addEventListener('input', checkPassword);
